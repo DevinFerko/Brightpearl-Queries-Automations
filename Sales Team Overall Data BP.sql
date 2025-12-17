c5a52e28b54f9dc1442962d0633b225b5b8f6654
Devin Ferko <devin.ferko@outlook.com>
2025-12-17 11:59:30 +0000

    Filter changes for customer name

--- Sales Team Overall Data BP.sql
+++ Sales Team Overall Data BP.sql
@@ -9,10 +9,14 @@ SELECT DISTINCT
     ,o.ord_channelId AS [Channel ID]
     ,o.ord_staffOwnerContactId AS [Staff Owner Contact ID]
     ,c.con_firstName + ' ' + c.con_lastName AS [Staff Owner Name]
+    ,CASE WHEN op.par_type = 'customer' THEN op.par_addressFullName END AS [Customer Name]
 FROM
     dbo.tblOrder AS o
 LEFT JOIN
     dbo.tblContact AS c ON o.ord_staffOwnerContactId = c.con_id
+LEFT JOIN
+    dbo.tblOrderParty AS op ON o.ord_id = op.par_ord_id AND op.par_type = 'customer'
 WHERE
     o.ord_orderStatusName = 'Invoiced'
+    AND op.par_type = 'customer'    
     AND o.ord_invoicetaxDate >= '2024-12-01'
