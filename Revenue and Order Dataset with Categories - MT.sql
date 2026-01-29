e1320e05c9611254cccb0c36f6ec75b449317e87
Devin Ferko <devin.ferko@outlook.com>
2026-01-29 14:39:02 +0000

    Additional query requested by Matt T

---
+++ Revenue and Order Dataset with Categories - MT.sql
@@ -0,0 +1,56 @@
+WITH data as (
+    SELECT 
+        CAST(ord_placedOn AS date) AS order_date,
+        
+        CAST(ord_invoicetaxDate AS date) AS invoice_tax_date, -- For Tax Dates, can also be used to filter for invoiced or not
+        CAST(ord_createdOn AS date) AS created_on_date, -- For created on Dates 
+        
+        ord_channelId, -- one half of revenue source calc (Brand)
+        ord_leadSourceId, -- second half (Brand)
+        
+        ord_id,
+        COUNT(DISTINCT ord_id) AS order_count, 
+        sum(ord_net) as net,
+        sum(ord_taxAmount) as texAmount,
+        sum(ord_baseNet) as baseNet,
+        sum(ord_baseTaxAmount) as baseTaxAmount,
+        sum(ord_baseTotal) as baseTotal,
+        sum(ord_total) as total
+    FROM dbo.tblOrder
+    WHERE CAST(ord_placedOn AS date) = '2026-01-27'
+    GROUP BY CAST(ord_placedOn AS date),
+        ord_channelId, ord_id, CAST(ord_invoicetaxDate AS date),CAST(ord_createdOn AS date), ord_leadSourceId, ord_net
+),
+
+
+channel as (
+    SELECT 
+        chn_id, 
+        chn_name 
+    FROM [dbo].[tblChannel]
+    Group By chn_id, chn_name),
+
+
+products as ( 
+    SELECT 
+        orl_ord_id,
+        orl_productID,
+        orl_productSku, 
+        orl_productName, 
+        orl_quantity,
+        
+        CASE -- Case statement for categories, in dbo.tblProductCustomField table
+            WHEN pcf_name = 'PCF_CAT1' THEN pcf_value -- Use PCF_CAT2, PCF_CAT3, PCF_CAT4 for other categories.
+        END AS primary_category 
+
+    FROM [dbo].[tblOrderLine]
+    LEFT JOIN dbo.tblProductCustomField ON orl_productId = pcf_prd_id
+    
+    )
+
+SELECT DISTINCT -- Added Distinct to remove any possible duplicated rows
+    * 
+FROM data d
+LEFT JOIN channel c ON d.ord_channelId = c.chn_id
+LEFT JOIN products p ON d.ord_id = p.orl_ord_id
+-- WHERE invoice_tax_date IS NOT NULL -- Will get orders with invoiced dates
