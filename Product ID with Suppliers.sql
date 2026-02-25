3a1ac640887fb1463397421260689bdf988fa0e9
Devin Ferko <devin.ferko@outlook.com>
2026-02-25 14:59:54 +0000

    Product custom field added

--- Product ID with Suppliers.sql
+++ Product ID with Suppliers.sql
@@ -1,6 +1,8 @@
 SELECT 
     psu_prd_id,
     psu_con_id,
-    con_organisationName
+    con_organisationName,
+    CASE WHEN pcf_name = 'PCF_CAT1' THEN pcf_value END AS [Primary Category]
 FROM dbo.tblProductSupplier
 LEFT JOIN dbo.tblContact ON psu_con_id = con_id
+LEFT JOIN dbo.tblProductCustomField ON psu_prd_id = pcf_prd_id
