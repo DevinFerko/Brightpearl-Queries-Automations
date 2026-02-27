4e6897554927682d81e1e2f9c996046b2f72934d
Devin Ferko <devin.ferko@outlook.com>
2026-02-27 11:02:09 +0000

    Inventory Detail update

--- Automations/Inventory Detail Export.sql
+++ Automations/Inventory Detail Export.sql
@@ -12,11 +12,14 @@ SELECT DISTINCT
     loc_groupingA AS [Aisle],
     loc_groupingB AS [Bay],
     loc_groupingC AS [Shelf],
-    loc_groupingD AS [Bin]
+    loc_groupingD AS [Bin],
+    gin_receivedOn AS [Received Date]
     
     -- Looking for Received Date
     --prd_createdOn Not
     --prd_updatedOn Not
+    -- gin_receivedOn Not ???????
+    -- gin_createdOn NOT
 
     
 FROM dbo.tblProduct
@@ -26,5 +29,8 @@ LEFT JOIN dbo.tblProductWarehouse ON prd_id = pwh_prd_id
 LEFT JOIN dbo.tblWarehouse ON pwh_whs_id = whs_id
 LEFT JOIN dbo.tblProductAvailabilityByLocation on prd_id = pal_prd_id
 LEFT JOIN dbo.tblLocation ON pal_loc_id = loc_id
+LEFT JOIN dbo.tblGoodsInNote ON prd_id = gin_productId
 WHERE prp_priceListId =1
-AND prd_identitySKU = '2/C626'
+AND pav_inStock IS NOT NULL AND pav_inStock <> 0
+--AND prd_identitySKU = '2/C626'
+--AND gin_receivedOn IS NOT NULL
