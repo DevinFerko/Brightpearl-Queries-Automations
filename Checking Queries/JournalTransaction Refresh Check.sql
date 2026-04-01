4ac51b8bb38fee9b96823fcfb2935425c33f9a7a
Devin Ferko <devin.ferko@outlook.com>
2026-04-01 14:10:00 +0100

    Marketing Dataset

--- Checking Queries/JournalTransaction Refresh Check.sql
+++ Checking Queries/JournalTransaction Refresh Check.sql
@@ -2,4 +2,4 @@ SELECT
     SUM(jtr_transactionAmount) AS transaction_amount
 FROM dbo.tblJournalTransaction
 LEFT JOIN dbo.tblJournal ON jou_id = jtr_jou_id
-WHERE jou_taxDate = '2025-09-03'
+WHERE jou_taxDate = '2026-03-16'
