22c0b0df0711fc7af42a39b6b95fef9b5ce29ba5
Devin Ferko <devin.ferko@outlook.com>
2026-06-12 14:21:56 +0100

    Weekly addition

--- Pipeline Revenue and Cancelled Orders.sql
+++ Pipeline Revenue and Cancelled Orders.sql
@@ -1,5 +1,4 @@
 SELECT DISTINCT 
-  *,
   CASE
     WHEN YEAR(ord_invoiceTaxDate) > YEAR(ord_placedOn)
       OR (
@@ -8,12 +7,25 @@ SELECT DISTINCT
       )
     THEN 1
     ELSE 0
-  END AS pipeline_order,
+  END AS pipeline_orders, 
+
+  -- Create similar case when for weekly pipeline values for granularity
+
+CASE
+    WHEN 
+            YEAR(ord_invoiceTaxDate) = YEAR(ord_placedOn)
+        AND DATEDIFF(day, '1900-01-01', ord_invoiceTaxDate) / 7 > DATEDIFF(day, '1900-01-01', ord_placedOn) / 7 -- Sets Trading Week to Monday to Sunday
+      
+    THEN 1
+    ELSE 0
+END AS pipeline_orders_weekly,
+
   CASE
     WHEN ord_orderStatusName IN ('Cancelled', 'Cancelled SO Refund') -- Are Needed ?? 'Damaged / Missing It', 'Incorrect Goods Rece', 'Refund Awaiting Info', 'Refund Due', 'Unconfirmed Cancelle', ' Unprocessed orders'
     THEN 1
     ELSE 0
-  END AS cancelled_order
+  END AS cancelled_order,
+  *
 FROM dbo.tblOrder
 LEFT JOIN dbo.tblOrderLine ON ord_id = orl_ord_id 
 WHERE 1=1
